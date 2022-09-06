744f54b3636506795c0a3740e0b4a4ec6cd65562
Jessica Chen <jessicachen012@gmail.com>
2022-09-06 16:18:13 -0700

    get different routes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const { PORT } = process.env;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'dist')));
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'dist', 'index.html')));
 
 app.listen(PORT, () => {
   console.log(`Client is served at localhost:${PORT}`);
