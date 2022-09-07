50c84b55ecd57a0cb145ddcfd77bd675ff14391b
Jessica Chen <jessicachen012@gmail.com>
2022-09-06 18:28:41 -0700

    login structure

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('username: ', username);
+  };
 
   return (
-    <h1>This is the Login page</h1>
+    <div>
+      <h1>This is the Login page</h1>
+      <form autoComplete="off" onSubmit={handleSubmit}>
+        <label htmlFor="username">
+          Username
+          <input
+            name="username"
+            value={username}
+            type="text"
+            required
+            onChange={(e) => { setUsername(e.target.value); }}
+          />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input
+            name="password"
+            value={password}
+            type="password"
+            required
+            onChange={(e) => { setPassword(e.target.value); }}
+          />
+        </label>
+        <input type="submit" value="Sign In" />
+      </form>
+      <p>Don't have an account? Sign up!</p>
+      <button type="button" onClick={() => { navigate('/signup'); }}>Sign up</button>
+    </div>
   );
 }
