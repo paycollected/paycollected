239a40805a435229f67f4d3776e2fb05b9c7221c
Jessica Chen <jessicachen012@gmail.com>
2022-09-06 18:28:19 -0700

    added login and join buttons

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
+  const navigate = useNavigate();
+
   return (
-    <h1>This is the Home page</h1>
+    <div>
+      <h1>This is the Home page</h1>
+      <button type="button" onClick={() => { navigate('/login'); }}>Login</button>
+      <button type="button" onClick={() => { navigate('/signup'); }}>Sign up</button>
+    </div>
   );
 }
