a918cf30142eb7408fdf165947e66c77e4684319
Jessica Chen <jessicachen012@gmail.com>
2022-09-19 13:36:34 -0700

    added MUI button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import Button from '@mui/material/Button';
 
 export default function Home() {
   const navigate = useNavigate();
@@ -7,8 +8,8 @@ export default function Home() {
   return (
     <div>
       <h1>This is the Home page</h1>
-      <button type="button" onClick={() => { navigate('/login'); }}>Login</button>
-      <button type="button" onClick={() => { navigate('/signup'); }}>Sign up</button>
+      <Button variant="contained" onClick={() => { navigate('/login'); }}>Login</Button>
+      <Button variant="contained" onClick={() => { navigate('/signup'); }}>Sign up</Button>
     </div>
   );
 }
