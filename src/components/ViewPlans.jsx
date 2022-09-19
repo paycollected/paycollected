357745f9c0bc76cb8a7442e7949b19d5d35bfc4d
Binh Nguyen <kbinhnguyen.nihongo@gmail.com>
2022-09-19 03:12:18 -0400

    Added comments to ViewPlans

--- src/components/ViewPlans.jsx
+++ src/components/ViewPlans.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 export default function ViewPlans() {
-  // will link to Stripe page, can cancel subscriptions
+  // this component will display all existing plans for this user (query from backend)
+  // it will also include a link to Stripe page, can cancel subscriptions
   return (
     <h1>This is the ViewSubscriptions page to list all subscriptions</h1>
   );
