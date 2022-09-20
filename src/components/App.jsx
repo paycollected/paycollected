d6bf92ac045c1b803bde44ca2dd1828c6843a966
Jessica Chen <jessicachen012@gmail.com>
2022-09-20 11:38:18 -0700

    pass setPlanToJoin to dashboard for logout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,7 +21,7 @@ function App() {
       <Route path="/" element={!user ? <Home /> : <Navigate to="/dashboard" />} />
       <Route path="/login" element={!user ? <Login setUser={setUser} planToJoin={planToJoin} /> : <Navigate to="/dashboard" />} />
       <Route path="/signup" element={!user ? <Signup setUser={setUser} planToJoin={planToJoin} /> : <Navigate to="/dashboard" />} />
-      <Route path="/dashboard" element={user ? <Dashboard username={user} setUser={setUser} /> : <Navigate to="/" />} />
+      <Route path="/dashboard" element={user ? <Dashboard username={user} setUser={setUser} setPlanToJoin={setPlanToJoin} /> : <Navigate to="/" />} />
       <Route
         path="/plan/create"
         element={
