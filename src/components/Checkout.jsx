dc457a32f73656cd3643b048dda8ad425915a50f
Jessica Chen <jessicachen012@gmail.com>
2022-09-27 12:04:08 -0700

    load stripe elements to checkout

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import {
+  useStripe, useElements, CardElement, Elements
+} from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 
-export default function Checkout() {
+const stripePromise = loadStripe(process.env.STRIPE_PUBLISHABLE_KEY);
+
+export default function Checkout({ stripeClientSecret }) {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const options = {
+    clientSecret: stripeClientSecret
+  };
+
   return (
-    <h1>This is the Checkout page</h1>
+    <Elements stripe={stripePromise} options={options}>
+      <h1>This is the Checkout page</h1>
+    </Elements>
   );
 }
