ce54bec6fd92d529f417633e72584be3e851815c
Jessica Chen <jessicachen012@gmail.com>
2022-09-27 12:03:27 -0700

    added stripe client secret state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,13 +18,17 @@ function App() {
   const [user, setUser] = useState(localStorage.getItem('username'));
   const [planToJoin, setPlanToJoin] = useState(null);
   const [showMagicLink, setShowMagicLink] = useState(false);
+  const [stripeClientSecret, setStripeClientSecret] = useState(null);
 
   return (
     <Routes>
       <Route path="/" element={!user ? <Home /> : <Navigate to="/dashboard" />} />
       <Route path="/login" element={!user ? <Login setUser={setUser} planToJoin={planToJoin} /> : <Navigate to="/dashboard" />} />
       <Route path="/signup" element={!user ? <Signup setUser={setUser} planToJoin={planToJoin} /> : <Navigate to="/dashboard" />} />
-      <Route path="/dashboard" element={user ? <Dashboard username={user} setUser={setUser} setPlanToJoin={setPlanToJoin} /> : <Navigate to="/" />} />
+      <Route
+        path="/dashboard"
+        element={user ? <Dashboard username={user} setUser={setUser} setPlanToJoin={setPlanToJoin} /> : <Navigate to="/" />}
+      />
       <Route
         path="/plan/create"
         element={
@@ -33,9 +37,21 @@ function App() {
             : <MagicLink planToJoin={planToJoin} setShowMagicLink={setShowMagicLink} />
           }
       />
-      <Route path="/join/:planId" element={!user ? <Home setPlanToJoin={setPlanToJoin} /> : <JoinPlan setPlanToJoin={setPlanToJoin} />} />
+      <Route
+        path="/join/:planId"
+        element={
+          !user
+            ? <Home setPlanToJoin={setPlanToJoin} />
+            : (
+              <JoinPlan
+                setPlanToJoin={setPlanToJoin}
+                setStripeClientSecret={setStripeClientSecret}
+              />
+            )
+          }
+      />
       <Route path="/cards" element={<Cards />} />
-      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/checkout" element={<Checkout stripeClientSecret={stripeClientSecret} />} />
       <Route path="/plan/all" element={<ViewPlans />} />
     </Routes>
   );
