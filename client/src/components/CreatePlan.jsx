e729472f75cbe50301cca0296425b43ecfffc44a
Binh Nguyen <kbinhnguyen.nihongo@gmail.com>
2022-10-07 02:40:49 -0400

    deleted console logs

--- client/src/components/CreatePlan.jsx
+++ client/src/components/CreatePlan.jsx
@@ -126,9 +126,6 @@ export default function CreatePlan({ setPlanToJoin, setShowMagicLink }) {
           max={nextMonthFullDate}
           onChange={(e) => { setStartDate(e.target.value); }}
         />
-        {console.log(fullDate)}
-        {console.log(nextMonthFullDate)}
-        {console.log(startDate)}
         <Button type="submit" variant="contained" disabled={loading}>Submit</Button>
       </form>
       <Button variant="contained" onClick={() => { navigate('/dashboard'); }}>Cancel</Button>
