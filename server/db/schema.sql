a350ffa38744184e0c8b9f790c6041f491445296
Binh Nguyen <kbinhnguyen.nihongo@gmail.com>
2022-10-08 21:25:14 -0400

    stripe cus ID not null in db schema

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -6,20 +6,17 @@ CREATE DATABASE paycollected;
 -- non-relational tables
 CREATE TABLE IF NOT EXISTS users (
   -- id SERIAL PRIMARY KEY,
-  username VARCHAR(100) PRIMARY KEY, -- shall we make this the primary key instead? the id field seems useless
+  username VARCHAR(100) PRIMARY KEY,
   first_name VARCHAR(100) NOT NULL,
   last_name VARCHAR (100) NOT NULL,
   password VARCHAR(200) NOT NULL, -- hashed
   email VARCHAR(100) NOT NULL UNIQUE,
-  s_cus_id VARCHAR(200) UNIQUE
-  -- credit card id
+  s_cus_id VARCHAR(200) NOT NULL UNIQUE
 );
 
 CREATE TYPE cycle_freq AS ENUM ('weekly', 'monthly', 'yearly');
 
 CREATE TABLE IF NOT EXISTS plans (
-  -- id SERIAL PRIMARY KEY,
-  -- owner_username VARCHAR(100) NOT NULL REFERENCES users(username),
   plan_name VARCHAR(50) NOT NULL,
   cycle_frequency CYCLE_FREQ NOT NULL,
   per_cycle_cost INTEGER NOT NULL,
