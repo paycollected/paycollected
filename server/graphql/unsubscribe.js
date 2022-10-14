0bf0666be41d2babfc1e99b388922da4b92db8e9
Binh Nguyen <kbinhnguyen.nihongo@gmail.com>
2022-10-13 21:11:55 -0400

    unsubscribe

--- server/graphql/unsubscribe.js
+++ server/graphql/unsubscribe.js
@@ -9,8 +9,8 @@ const stripe = stripeSDK(process.env.STRIPE_SECRET_KEY);
 export async function unsubscribe(subscriptionId, username) {
   // check that user calling this function is actually subscription owner
   // also check whether this user is planOwner
+
   const { rows } = await checkPlanOwner(subscriptionId, username);
-  console.log(rows);
   if (rows.length === 0) {
     // if signed-in user is not actually owner of this subscription, rows.length = 0
     throw new Error('Unauthorized request');
@@ -19,5 +19,6 @@ export async function unsubscribe(subscriptionId, username) {
     // --> cannot call this function but have to call unsubscribeAsOwner to transfer ownership
     throw new Error ('Wrong mutation call');
   }
-  return subscriptionId;
+    // await stripe.subscriptions.del(subscriptionId);
+    return subscriptionId;
 };
