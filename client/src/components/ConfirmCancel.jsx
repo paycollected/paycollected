c046249686ceae0083f3cfbfa8801f0b8077c86e
Binh Nguyen <kbinhnguyen.nihongo@gmail.com>
2022-10-14 01:14:53 -0400

    subscription cancellation as owner working front-end!

--- client/src/components/ConfirmCancel.jsx
+++ client/src/components/ConfirmCancel.jsx
@@ -7,8 +7,8 @@ import {
 import { ViewAllPlans as GET_ALL_PLANS } from '../graphql/queries.gql';
 
 export default function ConfirmCancel({ planToCancel: plan, setModal, user }) {
-  // const { plan, owner } = planToCancel;
   const { subscriptionId, planId, activeMembers, owner } = plan;
+  const [newOwner, setNewOwner] = useState(activeMembers.length > 0 ? activeMembers[0].username : null);
   const members = activeMembers.map((member) => (member.firstName));
   const membersStr = members.length > 1
     ? members.slice(0, -1).join(', ').concat(', and ', members[members.length - 1])
@@ -27,7 +27,8 @@ export default function ConfirmCancel({ planToCancel: plan, setModal, user }) {
   ] = useMutation(UNSUBSCRIBE_AS_OWNER, {
     onCompleted: () => {
       setModal(null);
-    }
+    },
+    refetchQueries: [{ query: GET_ALL_PLANS }, 'ViewAllPlans'],
   });
 
   const handleConfirmUnsubscribe = () => {
@@ -42,7 +43,7 @@ export default function ConfirmCancel({ planToCancel: plan, setModal, user }) {
     }
   };
 
-  console.log(plan);
+  console.log(newOwner);
 
   return (
     <div
@@ -54,11 +55,27 @@ export default function ConfirmCancel({ planToCancel: plan, setModal, user }) {
         padding: '1rem',
       }}
     >
+      <button type="button" onClick={() => { setModal(null); }}>Back</button>
       <h2>{plan.name}</h2>
       {members.length > 0 && (<p>{`with ${membersStr}`}</p>)}
       <p>{`x${plan.quantity}`}</p>
       <p>Are you sure you want to drop out of this plan?</p>
-      <button type="button" onClick={() => { setModal(null); }}>Back</button>
+      {owner.username === user && (
+        <div>
+          <p>Please transfer plan ownership to another member to proceed:</p>
+          {activeMembers.map((member) => (
+            <div key={member.username}>
+              <input
+                type="radio"
+                value={member.username}
+                checked={member.username === newOwner}
+                onChange={(e) => { setNewOwner(e.target.value); }}
+              />
+              {member.firstName}
+            </div>
+          ))}
+        </div>
+      )}
       <button type="button" onClick={handleConfirmUnsubscribe}>Confirm cancellation</button>
     </div>
   );
