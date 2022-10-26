af392908e1b4d6526ccb59b9402851e458cb4cce
Jessica Chen <jessicachen012@gmail.com>
2022-10-25 19:41:47 -0700

    moved handleSubmit and quantity check to ConfirmModifyQuant

--- client/src/components/ModifyQuantity.jsx
+++ client/src/components/ModifyQuantity.jsx
@@ -1,37 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Button, Input } from '@chakra-ui/react';
+import { Button, Badge } from '@chakra-ui/react';
 import { AddIcon, MinusIcon } from '@chakra-ui/icons';
-
-const regex = /[1-6]/;
+import ConfirmModifyQuant from './ConfirmModifyQuant.jsx';
 
 export default function ModifyQuantity({
-  quantity: originalQuant, setModal, setNewQuant, plan, setPlanToModify
+  quantity: originalQuant, plan
 }) {
   const navigate = useNavigate();
   const [quantity, setQuantity] = useState(originalQuant.toString());
-  const [inputErr, setInputErr] = useState(null);
-
-  useEffect(() => {
-    setInputErr(null);
-  }, [quantity]);
 
   // maybe allow only up to 6 units per plan
 
-  const handleSubmit = () => {
-    if (quantity === originalQuant.toString()) {
-      setInputErr('Please submit a quantity different from your original.');
-    } else if (!regex.test(quantity) || quantity.length > 1) {
-      setInputErr('Invalid input! Only 1 through 6 please.');
-    } else {
-      setPlanToModify(plan);
-      setNewQuant(Number(quantity));
-      setModal('confirmQuantChange');
-    }
-  };
   return (
     <div>
-      {inputErr && (<p>{inputErr}</p>)}
       {quantity === '0'
         ? (
           <Button onClick={() => { navigate(`/join/${plan.planId}`); }}>Join</Button>
@@ -50,24 +32,20 @@ export default function ModifyQuantity({
             >
               <MinusIcon />
             </Button>
-            <Input
-              type="text"
-              maxLength="1"
-              min="1"
-              max="6"
-              value={quantity}
-              onChange={(e) => {
-                setQuantity(e.target.value);
-                // setInputErr(null);
-              }}
-              style={{ width: '20px' }}
-            />
+            <Badge
+              mx="1"
+              width="3ch"
+              fontSize="1.2em"
+              colorScheme={quantity === originalQuant.toString() ? 'default' : 'red'}
+            >
+              {quantity}
+            </Badge>
             <Button
               size="xs"
               disabled={Number(quantity) >= 6}
               onClick={
                 () => {
-                  if (!isNaN(Number(quantity))) {
+                  if (!Number.isNaN(Number(quantity))) {
                     setQuantity((Number(quantity) + 1).toString());
                   }
                 }
@@ -75,12 +53,11 @@ export default function ModifyQuantity({
             >
               <AddIcon />
             </Button>
-            <Button
-              onClick={handleSubmit}
-              disabled={!!inputErr}
-            >
-              Change quantity
-            </Button>
+            <ConfirmModifyQuant
+              plan={plan}
+              originalQuant={originalQuant}
+              newQuantity={quantity}
+            />
           </div>
         )}
     </div>
