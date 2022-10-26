32a7ce08d392a003da7028334b17a8f5b082921e
Jessica Chen <jessicachen012@gmail.com>
2022-10-25 17:47:17 -0700

    chakra ui

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -2,7 +2,9 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { useForm } from 'react-hook-form';
-import { Button, Input, FormControl, FormLabel, FormErrorMessage } from '@chakra-ui/react';
+import {
+  Flex, Box, Heading, Button, Input, FormControl, FormLabel, FormErrorMessage
+} from '@chakra-ui/react';
 import { CreateUser as SIGN_UP } from '../graphql/mutations.gql';
 
 export default function Signup({ setUser, planToJoin, setEmail }) {
@@ -60,121 +62,130 @@ export default function Signup({ setUser, planToJoin, setEmail }) {
   };
 
   return (
-    <div>
-      <h1>This is the Signup page</h1>
-      <form
-        autoComplete="off"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <FormControl
-          isRequired
-          isInvalid={errors.firstName}
-        >
-          <FormLabel>First Name</FormLabel>
-          <Input
-            name="firstName"
-            placeholder="Enter First Name"
-            type="text"
-            {...register('firstName', { required: 'First name required' })}
-          />
-          {errors.firstName ? (
-            <div>
-              <FormErrorMessage>
-                {errors.firstName.message}
-              </FormErrorMessage>
-            </div>
-          ) : (
-            <div>&nbsp;</div>
-          )}
-        </FormControl>
-        <FormControl
-          isRequired
-          isInvalid={errors.lastName}
-        >
-          <FormLabel>Last Name</FormLabel>
-          <Input
-            name="lastName"
-            placeholder="Enter Last Name"
-            type="text"
-            {...register('lastName', { required: 'Last name required' })}
-          />
-          {errors.lastName ? (
-            <div>
-              <FormErrorMessage>
-                {errors.lastName.message}
-              </FormErrorMessage>
-            </div>
-          ) : (
-            <div>&nbsp;</div>
-          )}
-        </FormControl>
-        <FormControl
-          isRequired
-          isInvalid={errors.username  || errorMessage === 'Username already exists'}
-        >
-          <FormLabel>Username</FormLabel>
-          <Input
-            name="username"
-            placeholder="Enter Username"
-            type="text"
-            {...register('username', { required: 'Username required' })}
-          />
-          {errors.username ? (
-            <div>
-              <FormErrorMessage>
-                {errors.username.message}
-              </FormErrorMessage>
-            </div>
-          ) : (
-            <div>&nbsp;</div>
-          )}
-        </FormControl>
-        <FormControl
-          isRequired
-          isInvalid={errors.password || errorMessage === 'Passwords must match'}
-        >
-          <Input
-            name="password"
-            placeholder="Enter Password"
-            type="password"
-            {...register('password', { required: 'Password required' })}
-          />
-          {errors.password ? (
-            <div>
-              <FormErrorMessage>
-                {errors.password.message}
-              </FormErrorMessage>
-            </div>
-          ) : (
-            <div>&nbsp;</div>
-          )}
-        </FormControl>
+    <Flex width="full" align="center" justifyContent="center">
+      <Box p={2} my={8} width="60%" bg="white" borderRadius="15">
+        <Box textAlign="center">
+          <Heading>Sign Up</Heading>
+        </Box>
+        <Box my={4} textAlign="left">
+          <form
+            autoComplete="off"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <FormControl
+              isRequired
+              isInvalid={errors.firstName}
+            >
+              <FormLabel>First Name</FormLabel>
+              <Input
+                name="firstName"
+                placeholder="Enter First Name"
+                type="text"
+                {...register('firstName', { required: 'First name required' })}
+              />
+              {errors.firstName ? (
+                <FormErrorMessage>
+                  {errors.firstName.message}
+                </FormErrorMessage>
+              ) : (
+                <div>&nbsp;</div>
+              )}
+            </FormControl>
+            <FormControl
+              isRequired
+              isInvalid={errors.lastName}
+            >
+              <FormLabel>Last Name</FormLabel>
+              <Input
+                name="lastName"
+                placeholder="Enter Last Name"
+                type="text"
+                {...register('lastName', { required: 'Last name required' })}
+              />
+              {errors.lastName ? (
+                <FormErrorMessage>
+                  {errors.lastName.message}
+                </FormErrorMessage>
+              ) : (
+                <div>&nbsp;</div>
+              )}
+            </FormControl>
+            <FormControl
+              isRequired
+              isInvalid={errors.username || errorMessage === 'Username already exists'}
+            >
+              <FormLabel>Username</FormLabel>
+              <Input
+                name="username"
+                placeholder="Enter Username"
+                type="text"
+                {...register('username', { required: 'Username required' })}
+              />
+              {errors.username ? (
+                <FormErrorMessage>
+                  {errors.username.message}
+                </FormErrorMessage>
+              ) : (
+                <div>&nbsp;</div>
+              )}
+            </FormControl>
+            <FormControl
+              isRequired
+              isInvalid={errors.password || errorMessage === 'Passwords must match'}
+            >
+              <Input
+                name="password"
+                placeholder="Enter Password"
+                type="password"
+                {...register('password', { required: 'Password required' })}
+              />
+              {errors.password ? (
+                <FormErrorMessage>
+                  {errors.password.message}
+                </FormErrorMessage>
+              ) : (
+                <div>&nbsp;</div>
+              )}
+            </FormControl>
+            <FormControl
+              isRequired
+              isInvalid={errors.password2 || errorMessage === 'Passwords must match'}
+            >
+              <Input
+                name="password2"
+                placeholder="Confirm Password"
+                type="password"
+                {...register('password2', { required: 'Please enter password again' })}
+              />
+              {errors.password2 ? (
+                <FormErrorMessage>
+                  {errors.password2.message}
+                </FormErrorMessage>
+              ) : (
+                <div>&nbsp;</div>
+              )}
+            </FormControl>
+            <FormControl
+              isRequired
+              isInvalid={errors.email || errorMessage === 'Email already exists'}
+            >
+              <Input
+                name="email"
+                placeholder="Enter Email"
+                type="email"
+                {...register('email', { required: 'Email required' })}
+              />
+              {errorMessage.length > 0 ? (
+                <FormErrorMessage>{errorMessage}</FormErrorMessage>
+              ) : (
+                <div>&nbsp;</div>
+              )}
 
-        <Input
-          name="password2"
-          placeholder="Confirm Password"
-          required
-          type="password"
-          {...register('password2', { required: 'Enter password again' })}
-          error={!!errors?.password2}
-          helperText={errors?.password2 ? errors.password2.message : ' '}
-        />
-        <Input
-          name="email"
-          placeholder="Enter Email"
-          required
-          type="email"
-          {...register('email', { required: 'Email required' })}
-          error={!!errors?.email}
-          helperText={errors?.email ? errors.email.message : ' '}
-        />
-        {errorMessage.length > 0 ? (
-          <div>{errorMessage}</div>
-        ) : (
-          <div>&nbsp;</div>
-        )}
-        <Button type="submit" isLoading={loading}>Join</Button>
-      </form>
-    </div>
+            </FormControl>
+            <Button type="submit" isLoading={loading}>Join</Button>
+          </form>
+        </Box>
+      </Box>
+    </Flex>
   );
 }
