17857332be52faa42686bfc89ac54c8786a4ad3e
Jessica Chen <jessicachen012@gmail.com>
2022-10-25 17:52:17 -0700

    chakra ui styling, modify quantity only if joined already, add link to copy

--- client/src/components/ViewPlans.jsx
+++ client/src/components/ViewPlans.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useQuery, useMutation } from '@apollo/client';
-import { Button } from '@chakra-ui/react';
+import {
+  Flex, Box, Grid, GridItem, Heading, Button, useClipboard
+} from '@chakra-ui/react';
 import { ViewAllPlans as GET_ALL_PLANS } from '../graphql/queries.gql';
 import { EditPayment as EDIT_PAYMENT } from '../graphql/mutations.gql';
 import ConfirmCancel from './ConfirmCancel.jsx';
@@ -13,7 +15,9 @@ export default function ViewPlans({ user }) {
   const navigate = useNavigate();
   const [modal, setModal] = useState(null);
   const [planToModify, setPlanToModify] = useState(null);
+  const [planToCopy, setPlanToCopy] = useState(null);
   const [newQuant, setNewQuant] = useState(null);
+  const { hasCopied, onCopy } = useClipboard(`${process.env.CLIENT_HOST}:${process.env.SERVER_PORT}/join/${planToCopy}`);
 
   const { loading, data, error } = useQuery(GET_ALL_PLANS, {
     fetchPolicy: 'network-only',
@@ -38,75 +42,85 @@ export default function ViewPlans({ user }) {
 
   return (
     <div>
-      <h1>This is the ViewSubscriptions page to list all subscriptions</h1>
       {modal === 'confirmCancel' && (<ConfirmCancel plan={planToModify} setModal={setModal} user={user} />)}
       {modal === 'confirmQuantChange' && (<ConfirmModifyQuant plan={planToModify} setModal={setModal} newQuantity={newQuant} />)}
       {modal === 'confirmDeletePlan' && (<ConfirmDeletePlan plan={planToModify} setModal={setModal} />)}
       <Button onClick={() => { navigate('/dashboard'); }}>Dashboard</Button>
       <Button onClick={() => { submitEditPayment(); }}>Manage Payment Methods</Button>
-      {data
-        && (data.viewAllPlans.map((plan) => (
-          <div
-            key={plan.planId}
-            style={{
-              display: 'flex',
-              justifyContent: 'space-between',
-            }}
-          >
-            <div>
-              <h2>{plan.name}</h2>
-              <div>
-                Owned by:&nbsp;
-                {plan.owner.username !== user ? plan.owner.firstName.concat(' ', plan.owner.lastName) : 'you'}
+      <Flex justifyContent="center">
+        <Box p={2} my={8} width="60%" bg="white" borderRadius="15">
+          <Box textAlign="center">
+            <Heading>Your Subscriptions</Heading>
+          </Box>
+          {data
+            && (data.viewAllPlans.map((plan) => (
+              <div key={plan.name}>
+                <Grid templateColumns="repeat(3, 1fr)" gap={3} mb={3}>
+                  <GridItem colSpan={2} textAlign="left">
+                    <Heading size="xl">{plan.name}</Heading>
+                    <div>
+                      Owned by:&nbsp;
+                      {plan.owner.username !== user ? plan.owner.firstName.concat(' ', plan.owner.lastName) : 'you'}
+                    </div>
+                    <div>{`Total Plan Cost: $${plan.perCycleCost} ${plan.cycleFrequency.toLowerCase()}`}</div>
+                    <div>{`Your quantity: ${plan.quantity}`}</div>
+                    {plan.activeMembers.length > 0 && (
+                      <>
+                        <div>Others on this plan:</div>
+                        <ul>
+                          {plan.activeMembers.map((member) => (
+                            <li key={member.username}>{`${member.firstName} ${member.lastName} x ${member.quantity}`}</li>
+                          ))}
+                        </ul>
+                      </>
+                    )}
+                    {plan.activeMembers.length === 0
+                      && (<div>There are currently no other members on this plan.</div>)}
+                    {planToCopy === plan.planId ? (
+                      <Box>
+                        {`Link to join: ${process.env.CLIENT_HOST}:${process.env.SERVER_PORT}/join/${plan.planId}`}
+                        <Button variant="outline" size="sm" onClick={onCopy}>
+                          {hasCopied ? 'Copied' : 'Copy'}
+                        </Button>
+                      </Box>
+                    ) : (
+                      <Button variant="outline" size="sm" onClick={() => setPlanToCopy(plan.planId)}>Show Link</Button>
+                    )}
+                  </GridItem>
+                  <GridItem colSpan={1} textAlign="center">
+                    <ModifyQuantity
+                      quantity={plan.quantity}
+                      setModal={setModal}
+                      setNewQuant={setNewQuant}
+                      plan={plan}
+                      setPlanToModify={setPlanToModify}
+                    />
+                    <br></br>
+                    {(plan.owner.username !== user
+                      || (plan.owner.username === user && plan.activeMembers.length > 0))
+                      && (
+                        <Button
+                          type="button"
+                          onClick={() => { handleSubsModification(plan, 'confirmCancel'); }}
+                        >
+                          Cancel subscription
+                        </Button>
+                      )}
+                    {plan.owner.username === user && (
+                      <Button
+                        onClick={() => { handleSubsModification(plan, 'confirmDeletePlan'); }}
+                      >
+                        Delete plan
+                      </Button>
+                    )}
+                  </GridItem>
+                </Grid>
               </div>
-              <div>{`Total Plan Cost: $${plan.perCycleCost} ${plan.cycleFrequency.toLowerCase()}`}</div>
-              <div>{`Your quantity: ${plan.quantity}`}</div>
-              {plan.activeMembers.length > 0 && (
-                <>
-                  <div>Others on this plan:</div>
-                  <ul>
-                    {plan.activeMembers.map((member) => (
-                      <li key={member.username}>{`${member.firstName} ${member.lastName} x ${member.quantity}`}</li>
-                    ))}
-                  </ul>
-                </>
-              )}
-              {plan.activeMembers.length === 0
-                && (<div>There are currently no other members on this plan.</div>)}
-            </div>
-            <div style={{ display: 'grid', alignContent: 'center', gap: '1.5rem' }}>
-              <ModifyQuantity
-                quantity={plan.quantity}
-                setModal={setModal}
-                setNewQuant={setNewQuant}
-                plan={plan}
-                setPlanToModify={setPlanToModify}
-              />
-              {(plan.owner.username !== user
-                || (plan.owner.username === user && plan.activeMembers.length > 0))
-                && (
-                  <button
-                    type="button"
-                    onClick={() => { handleSubsModification(plan, 'confirmCancel'); }}
-                    style={{ width: '150px', justifySelf: 'center' }}
-                  >
-                    Cancel subscription
-                  </button>
-                )}
-              {plan.owner.username === user && (
-                <button
-                  type="button"
-                  onClick={() => { handleSubsModification(plan, 'confirmDeletePlan'); }}
-                  style={{ width: '150px', justifySelf: 'center' }}
-                >
-                  Delete plan
-                </button>
-              )}
-            </div>
-          </div>
-        )))}
-      {data && data.viewAllPlans.length === 0
-        && (<div>You are not enrolled in any plans at the moment.</div>)}
+            )))}
+          {data && data.viewAllPlans.length === 0
+            && (<div>You are not enrolled in any plans at the moment.</div>)}
+        </Box>
+      </Flex>
     </div>
   );
 }
