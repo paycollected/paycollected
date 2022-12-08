2695fc808eb3f5c4ec0f66a1888c373db6a8af91
Jessica Chen <jessicachen012@gmail.com>
2022-12-08 15:13:15 -0800

    passed props to NavBar

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { Heading, Divider } from '@chakra-ui/react';
+import NavBar from '../../components/NavBar.jsx';
 
-export default function Home({ setPlanToJoin, }) {
+export default function Home({ user, setUser, setPlanToJoin }) {
   const { planId } = useParams();
 
   useEffect(() => {
@@ -13,6 +14,7 @@ export default function Home({ setPlanToJoin, }) {
 
   return (
     <div>
+      <NavBar user={user} setUser={setUser} setPlanToJoin={setPlanToJoin} />
       <h1>This is the Home page</h1>
       <Heading my={5}>
         Info About App
