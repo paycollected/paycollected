e20f812cac70ffbe467e3373b3145bcaa2bd281f
Binh Nguyen <kbinhnguyen.nihongo@gmail.com>
2022-12-10 01:52:56 -0500

    string format for display

--- client/src/pages/dashboard/PlansTable.jsx
+++ client/src/pages/dashboard/PlansTable.jsx
@@ -35,15 +35,17 @@ export default function PlansTable({ plans }) {
             planId, name, owner, isOwner, nextBillDate, cycleFrequency, perCycleCost, quantity,
             selfCost,
           } = plan;
+          const nbdAsArr = nextBillDate.split('-');
+          const fNbd = `${nbdAsArr[1]}/${nbdAsArr[2]}/${nbdAsArr[0]}`;
           return (
             <Tr key={planId}>
               <Td>{name}</Td>
               {isOwner && (<Td>You</Td>)}
               {!isOwner && (<Td>{`${owner.firstName} ${owner.lastName[0]}.`}</Td>)}
               <Td>{selfCost}</Td>
-              <Td>{nextBillDate}</Td>
+              <Td>{fNbd}</Td>
               <Td>{perCycleCost}</Td>
-              <Td>{cycleFrequency.toLowerCase()}</Td>
+              <Td>{cycleFrequency[0].concat(cycleFrequency.slice(1).toLowerCase())}</Td>
               <Td>{quantity}</Td>
               <Td>
                 <IconButton
