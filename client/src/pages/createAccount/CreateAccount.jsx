32899175ddc9ce227bfff3f7848f649498563bf6
Jessica Chen <jessicachen012@gmail.com>
2022-12-17 13:44:26 -0800

    auto focus on first input field

--- client/src/pages/createAccount/CreateAccount.jsx
+++ client/src/pages/createAccount/CreateAccount.jsx
@@ -109,6 +109,7 @@ export default function CreateAccount({ setUser }) {
                   <Input
                     name="fullName"
                     type="text"
+                    autoFocus="true"
                     {...register('fullName', { required: 'First and last name required' })}
                   />
                   {errors.fullName && (
