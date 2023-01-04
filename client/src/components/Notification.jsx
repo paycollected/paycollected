e6cbb7e4d5a58365d9660e827c7d8dbcd28740c0
Binh Nguyen <kbinhnguyen.nihongo@gmail.com>
2023-01-04 02:28:48 -0500

    format date for timestamp

--- client/src/components/Notification.jsx
+++ client/src/components/Notification.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { format } from 'date-fns';
 import {
   PopoverBody, Divider, Container, Text, Button, VStack, Flex, Box,
 } from '@chakra-ui/react';
@@ -7,6 +8,8 @@ export default function Notification({ notification, deleteNotification }) {
   const {
     id, content, subject, createdAt,
   } = notification;
+
+  const date = new Date(createdAt);
 
   return (
     <Container px={2} pt={1}>
@@ -25,7 +28,12 @@ export default function Notification({ notification, deleteNotification }) {
                 Dismiss
               </Button>
             </Flex>
-            <Text fontWeight="400" fontSize="sm" color="gray.700">{content}</Text>
+            <Container w="100%" px={0}>
+              <Text fontWeight="400" fontSize="sm" color="gray.700">{content}</Text>
+            </Container>
+            <Container w="100%" px={0}>
+              <Text color="gray.600" fontSize="sm" fontWeight="400">{`${format(date, 'EEE, MMM do, yyyy')} at ${format(date, 'K:mm bbbb')}`}</Text>
+            </Container>
           </VStack>
         </Container>
       </PopoverBody>
