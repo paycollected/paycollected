4e5c8b0ea1ab22f4802f9ae9cf80b84010a585e8
Binh Nguyen <kbinhnguyen.nihongo@gmail.com>
2023-01-08 21:03:52 -0500

    create plan form styling

--- client/src/pages/dashboard/createPlan/CreatePlanForm.jsx
+++ client/src/pages/dashboard/createPlan/CreatePlanForm.jsx
@@ -50,107 +50,109 @@ export default function CreatePlanForm({ createNewPlan, onCloseDrawer, loading,
         </Text>
       </DrawerHeader>
       <DrawerBody>
-        <Stack spacing="3">
+        <Stack spacing="6">
           <Box>
             Create a new plan with your friends. Fill in the details below to get started.
           </Box>
-          <form
-            autoComplete="off"
-            id="create-plan-form"
-            onSubmit={handleSubmit(onSubmit)}
-          >
-            <FormControl
-              isRequired
-              isInvalid={errors.planName}
+          <Box w="75%">
+            <form
+              autoComplete="off"
+              id="create-plan-form"
+              onSubmit={handleSubmit(onSubmit)}
             >
-              <FormLabel htmlFor="planName">Plan Name</FormLabel>
-              <Input
-                name="planName"
-                type="text"
-                autoFocus
-                {...register('planName', {
-                  required: 'Plan name required',
-                  minLength: { value: 5, message: 'Plan name must be at least 5 characters'},
-                  maxLength: { value: 100, message: 'Plan name must be no more than 100 characters'},
-                  setValueAs: (name) => name.trim(),
-                })}
-              />
-              {errors.planName ? (
-                <FormErrorMessage>
-                  {errors.planName.message}
-                </FormErrorMessage>
-              ) : (
-                <>&nbsp;</>
-              )}
-            </FormControl>
-            <FormControl
-              isRequired
-              isInvalid={errors.perCycleCost}
-            >
-              <FormLabel htmlFor="perCycleCost">Per Cycle Cost</FormLabel>
-              <InputGroup>
-                <InputLeftElement children="$" />
+              <FormControl
+                isRequired
+                isInvalid={errors.planName}
+              >
+                <FormLabel htmlFor="planName" textStyle="formLabel" fontWeight={600}>Plan Name</FormLabel>
                 <Input
-                  name="perCycleCost"
+                  name="planName"
                   type="text"
-                  {...register('perCycleCost', {
-                    required: 'Enter total cost per pay cycle',
-                    validate: (val) => Number(val) >= 10 && Number(val) <= 999999.99 || 'Cost must be at least $10 and no more than $999,999.99',
-                    pattern: {
-                      value: /^(([1-9]([0-9]+)\.([0-9]{2}))|([1-9]([0-9]+)))$/,
-                      message: 'Input must only contain digits, and a period (optional)',
-                    },
+                  autoFocus
+                  {...register('planName', {
+                    required: 'Plan name required',
+                    minLength: { value: 5, message: 'Plan name must be at least 5 characters'},
+                    maxLength: { value: 100, message: 'Plan name must be no more than 100 characters'},
+                    setValueAs: (name) => name.trim(),
                   })}
                 />
-              </InputGroup>
-              <FormHelperText>
-                Total cost per billing cycle for everyone in the plan (i.e., $100 a week)
-              </FormHelperText>
-              {errors.perCycleCost ? (
-                <FormErrorMessage>
-                  {errors.perCycleCost.message}
-                </FormErrorMessage>
-              ) : (
-                <>&nbsp;</>
-              )}
-            </FormControl>
-            <FormControl
-              isRequired
-              isInvalid={errors.cycleFrequency}
-            >
-              <FormLabel htmlFor="billingFrequency">Billing Frequency</FormLabel>
-              <Select
-                variant="outline"
-                name="billingFrequency"
-                {...register('billingFrequency', { required: 'Select billing frequency' })}
+                {errors.planName ? (
+                  <FormErrorMessage>
+                    {errors.planName.message}
+                  </FormErrorMessage>
+                ) : (
+                  <>&nbsp;</>
+                )}
+              </FormControl>
+              <FormControl
+                isRequired
+                isInvalid={errors.perCycleCost}
               >
-                {['Weekly', 'Monthly', 'Yearly'].map((freq) => (
-                  <option key={freq} value={freq}>
-                    {freq}
-                  </option>
-                ))}
-              </Select>
-              {errors.cycleFrequency ? (
-                <FormErrorMessage>
-                  {errors.cycleFrequency.message}
-                </FormErrorMessage>
-              ) : (
-                <>&nbsp;</>
-              )}
-            </FormControl>
-            <FormControl
-              isRequired
-            >
-              <FormLabel>Start Date</FormLabel>
-              <Input
-                type="date"
-                value={startDate}
-                min={fullDate}
-                max={nextMonthFullDate}
-                onChange={(e) => { setStartDate(e.target.value); }}
-              />
-            </FormControl>
-          </form>
+                <FormLabel htmlFor="perCycleCost" textStyle="formLabel" fontWeight={600}>Per Cycle Cost</FormLabel>
+                <InputGroup>
+                  <InputLeftElement children="$" />
+                  <Input
+                    name="perCycleCost"
+                    type="text"
+                    {...register('perCycleCost', {
+                      required: 'Enter total cost per pay cycle',
+                      validate: (val) => Number(val) >= 10 && Number(val) <= 999999.99 || 'Cost must be at least $10 and no more than $999,999.99',
+                      pattern: {
+                        value: /^(([1-9]([0-9]+)\.([0-9]{2}))|([1-9]([0-9]+)))$/,
+                        message: 'Input must only contain digits, and a period (optional)',
+                      },
+                    })}
+                  />
+                </InputGroup>
+                <FormHelperText>
+                  Total cost per billing cycle (i.e., $100 a week)
+                </FormHelperText>
+                {errors.perCycleCost ? (
+                  <FormErrorMessage>
+                    {errors.perCycleCost.message}
+                  </FormErrorMessage>
+                ) : (
+                  <>&nbsp;</>
+                )}
+              </FormControl>
+              <FormControl
+                isRequired
+                isInvalid={errors.cycleFrequency}
+              >
+                <FormLabel htmlFor="billingFrequency" textStyle="formLabel" fontWeight={600}>Billing Frequency</FormLabel>
+                <Select
+                  variant="outline"
+                  name="billingFrequency"
+                  {...register('billingFrequency', { required: 'Select billing frequency' })}
+                >
+                  {['Weekly', 'Monthly', 'Yearly'].map((freq) => (
+                    <option key={freq} value={freq}>
+                      {freq}
+                    </option>
+                  ))}
+                </Select>
+                {errors.cycleFrequency ? (
+                  <FormErrorMessage>
+                    {errors.cycleFrequency.message}
+                  </FormErrorMessage>
+                ) : (
+                  <>&nbsp;</>
+                )}
+              </FormControl>
+              <FormControl
+                isRequired
+              >
+                <FormLabel textStyle="formLabel" fontWeight={600}>Start Date</FormLabel>
+                <Input
+                  type="date"
+                  value={startDate}
+                  min={fullDate}
+                  max={nextMonthFullDate}
+                  onChange={(e) => { setStartDate(e.target.value); }}
+                />
+              </FormControl>
+            </form>
+          </Box>
         </Stack>
       </DrawerBody>
       <DrawerFooter borderTopWidth="1px">
